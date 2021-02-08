f16dffc20dcb1136d927d2f238c57741b398de35
Georgiana Sfeclis <georgiana.sfeclis@bf1systems.com>
2021-02-08 12:40:27 +0000

    Clear Screen command

---
+++ Interpres_FrontEnd/Commands/ClrCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Interpreter.IO;
+using Interpreter.Tokens.commands;
+
+namespace Interpres_FrontEnd.Commands
+{
+    class ClrCommand : Command
+    {
+        public override object Execute(object[] args, Workspace workspace)
+        {
+            try
+            {
+                workspace.commandLog = new LinkedList<string>();
+                Interpres.MainForm.singleton.UpdateScrollback();
+                Console.Clear();
+                return "Success";
+            }
+            catch (Exception e)
+            {
+                return "Screen Cleared";
+
+            }
+        }
+
+        public override string GetInputString()
+        {
+            return "clr";
+        }
+    }
+}
--- Interpres_FrontEnd/MainForm.cs
+++ Interpres_FrontEnd/MainForm.cs
@@ -320,6 +320,7 @@ namespace Interpres
             {
                 CommandTokenizer commandTokenizer = new CommandTokenizer();
                 commandTokenizer.RegisterCommand(new PlotCommand());
+                commandTokenizer.RegisterCommand(new ClrCommand());
                 TokenizerService tokenizerService = new TokenizerService(commandTokenizer);
                 var tokens = tokenizerService.GetTokens(input);
                 workspace.commandLog.AddLast(">> " + input);
